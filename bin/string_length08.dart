c30757d203105a436800095c077e0ebf0dca5150
maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-04 17:09:22 +0500

    08

--- bin/string_length08.dart
+++ bin/string_length08.dart
@@ -7,6 +7,15 @@
     Returns:
         str: answer
 */
+String func(String s) {
+  String;
+  if (s.length % 2 == 0) {
+    return s[1 * 2 - 1] + 'd';
+  }
+  return s[s.length ~/ 2];
+}
+
 void main() {
+  print(func('code'));
   // write your code here
 }
